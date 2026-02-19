9c3091db0c036707c19eeccac7817a7feae4d011
Null <VectorZeroAI@gmail.com>
2026-02-19 17:23:49 +0100

    chore: cleaned imports up

--- ai_diagnos_lsp/AnalysisSubsystem/analysers/CrossFileAnalyser.py
+++ ai_diagnos_lsp/AnalysisSubsystem/analysers/CrossFileAnalyser.py
@@ -3,6 +3,7 @@
 The cross file analysis worker thread
 """
 from __future__ import annotations
+
 from typing import TYPE_CHECKING, TypedDict
 
 from pathlib import Path
@@ -11,15 +12,10 @@ import logging
 
 from pygls.workspace import TextDocument
 
-from lsprotocol import types
-
 from ai_diagnos_lsp.AnalysisSubsystem.analysers.chains.GeneralDiagnosticsPydanticOutputParser import GeneralDiagnosticsOutputParserFactory
 from ai_diagnos_lsp.utils.analyser.chain_invoker import chain_invoker_function
+from ai_diagnos_lsp.utils.analyser.llm_generator import LlmFactoryWithConfig
 
-from .chains.LLM.BasicOmniproviderLLM import BasicOmniproviderLLMFactory
-from .chains.LLM.BasicGeminiLLM import BasicGeminiLlmFactory
-from .chains.LLM.BasicGroqLLM import BasicGroqLLMFactory
-from .chains.LLM.BasicOpenrouterLLM import OpenrouterLlmFactory
 from .chains.PromptObjekts.CrossFileAnalysisPrompt import CrossFileAnalysisPromptFactory
 
 
@@ -57,47 +53,8 @@ def CrossFileAnalyserWorkerThread(ls: AIDiagnosLSP, file: TextDocument | Path):
 
         if LOG:
             logging.info("Cross File analyser Worker thread started")
-        try:
-            if ls.config["use_omniprovider"]:
 
-                llm = BasicOmniproviderLLMFactory(
-                        model_openrouter=ls.config["model_openrouter"],
-                        api_key_openrouter=ls.config["api_key_openrouter"],
-                        api_key_gemini=ls.config["api_key_gemini"],
-                        model_gemini=ls.config["model_gemini"],
-                        fallback_models_gemini=ls.config.get("fallback_models_gemini"),
-                        api_key_groq=ls.config["api_key_groq"],
-                        model_groq=ls.config["model_groq"],
-                        fallback_models_groq=ls.config.get("fallback_models_groq")
-                        )
-
-            elif ls.config["use_gemini"]:
-
-                llm = BasicGeminiLlmFactory(
-                        api_key_gemini=ls.config["api_key_gemini"],
-                        model_gemini=ls.config["model_gemini"],
-                        fallback_gemini_models=ls.config.get("fallback_models_gemini")
-                        )
-
-            elif ls.config["use_openrouter"]:
-                llm = OpenrouterLlmFactory(
-                        model_openrouter=ls.config["model_openrouter"],
-                        api_key_openrouter=ls.config["api_key_openrouter"]
-                        )
-
-            elif ls.config['use_groq']:
-                llm = BasicGroqLLMFactory(
-                        model_groq=ls.config['model_groq'],
-                        api_key_groq=ls.config['api_key_groq'],
-                        fallback_models_groq=ls.config['fallback_models_groq']
-                        )
-            else:
-                ls.window_show_message(types.ShowMessageParams(types.MessageType(1), "INVALID CONFIGURATION RECIEVED. One of use parameters must be true !"))
-                raise RuntimeError("INVALID CONFIGURATION RECEIVED. One of use parameters must be true !")
-
-
-        except KeyError as e:
-            raise RuntimeError(f"lines 49-86, Cross file analyser thread, Key error {e}") from e
+        llm = LlmFactoryWithConfig(ls.config)
 
         prompt = CrossFileAnalysisPromptFactory()
 

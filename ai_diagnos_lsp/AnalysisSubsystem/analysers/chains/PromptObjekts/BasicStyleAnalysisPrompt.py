e231751b13da3a784fcf527ae1b18edf1a78e24a
Null <VectorZeroAI@gmail.com>
2026-02-20 10:43:47 +0100

    feat: created the basic style analysers prompt object

---
+++ ai_diagnos_lsp/AnalysisSubsystem/analysers/chains/PromptObjekts/BasicStyleAnalysisPrompt.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from langchain_core.prompts import ChatPromptTemplate
+
+from .prompts.style_analysis_prompts import BASIC_STYLE_ANALYSIS_PROMPT
+
+def BasicLogicAnalysisPromptFactory() -> ChatPromptTemplate:
+    """
+    This is the Prompt Objekt factory for the chain.
+    I cant make it just an importable class, because if I do, 
+    I would still have to make an instanse of it for it to actually read the prompt. 
+    The prompt is stored in a separate file, at the prompts directory. 
+    """
+
+    LogicAnalysisPrompt = ChatPromptTemplate.from_messages([
+        ("system", f"{BASIC_STYLE_ANALYSIS_PROMPT}"),
+        ("human", "\n{{{file_content}}}\n\n"),
+        ], template_format="mustache")
+    return LogicAnalysisPrompt

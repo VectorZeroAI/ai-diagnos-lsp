2ca801719ea1804cc494fcf450648b374c06de2f
Null <VectorZeroAI@gmail.com>
2026-02-20 13:46:55 +0100

    started integrating the repairs step into the chains

--- ai_diagnos_lsp/AnalysisSubsystem/analysers/BasicDiagnoseFunction.py
+++ ai_diagnos_lsp/AnalysisSubsystem/analysers/BasicDiagnoseFunction.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
+from langchain_core.runnables import Runnable, RunnableLambda
 from pygls.workspace import TextDocument
 from pathlib import Path
 
@@ -9,6 +10,7 @@ from ai_diagnos_lsp.AnalysisSubsystem.analysers.chains.GeneralDiagnosticsPydanti
 from ai_diagnos_lsp.AnalysisSubsystem.analysers.chains.PromptObjekts.BasicAnalysisPrompt import BasicAnalysisPromptFactory
 from ai_diagnos_lsp.utils.analyser.chain_invoker import chain_invoker_function_basic
 from ai_diagnos_lsp.utils.analyser.llm_generator import LlmFactoryWithConfig
+from ai_diagnos_lsp.utils.json_repair import optional_repair_json
 
 if TYPE_CHECKING:
     from ai_diagnos_lsp.AIDiagnosLSPClass import AIDiagnosLSP
@@ -23,7 +25,9 @@ def BasicDiagnoseFunctionWorker(document: TextDocument | Path, ls: AIDiagnosLSP)
         prompt = BasicAnalysisPromptFactory()
         output = GeneralDiagnosticsOutputParserFactory()
 
-        chain = prompt | llm | output 
+        repairs = RunnableLambda(optional_repair_json)
+
+        chain: Runnable[Any, Any] = prompt | llm | repairs | output 
 
         chain_invoker_function_basic(document, ls.config, chain, ls, 'Basic')
 

aa2a2c341a2008fbb361c9fb02f0f312d1eee0fa
Null <VectorZeroAI@gmail.com>
2026-02-21 10:01:50 +0100

    fix repair json function now handles markdown prefixing of the json body

--- ai_diagnos_lsp/utils/json_repair.py
+++ ai_diagnos_lsp/utils/json_repair.py
@@ -4,26 +4,70 @@ from fix_busted_json import repair_json, can_parse_json # pyright: ignore
 # I use pyright in strict mode, and the developer of the library doesnt, so pyright always complains because of missing type hints
 from langchain_core.messages import AIMessage
 
+import os
+import logging
+
+if os.getenv('AI_DIAGNOS_LOG') is not None:
+    LOG = True
+else:
+    LOG = False # pyright: ignore
+
 def optional_repair_json(input_msg: AIMessage) -> AIMessage:
     """
     Takes AIMessage in, fixes json contents, gives AIMessage out. 
     """
-    if isinstance(input_msg.content, str): # pyright: ignore
-        content = input_msg.content
-    else:
-        if isinstance(input_msg.content[0], str):
-            content = "\n".join(input_msg.content)
-        elif isinstance(input_msg.content[0], dict):
-            content = "\n".join(s['text'] for s in input_msg.content if 'text' in s)
+    try:
+        if LOG:
+            logging.info("json repairs tool started")
+            logging.info(f"The content of AIMessage is the following : {input_msg.content}")
+
+        if isinstance(input_msg.content, str): # pyright: ignore
+            content = input_msg.content
+
         else:
+            if isinstance(input_msg.content, list):
+
+                if len(input_msg.content) > 0:
+
+                    if isinstance(input_msg.content[0], str):
+                        content = "\n".join(input_msg.content)
+
+                    elif isinstance(input_msg.content[0], dict):
+                        content = ""
+                        for i in input_msg.content:
+                            if i.get('type') == "text":
+                                if i.get('text') is not None:
+                                    content = content + i.get('text')
+                                else:
+                                    continue
+                    else:
+                        return input_msg
+                else:
+                    return input_msg
+            else:
+                return input_msg
+        
+        assert content is str
+        if can_parse_json(content):
             return input_msg
-    
-    if can_parse_json(content):
+        else:
+            if content.startswith('`'):
+                content.strip('`')
+            if content.startswith('json'):
+                content = content[4:]
+            try:
+                repaired_content: str = repair_json(content)
+                if isinstance(repaired_content, str):
+                    return AIMessage(content=repaired_content)
+                else:
+                    return input_msg
+            except Exception as e:
+                if LOG:
+                    logging.error(f"the repair json function call encoutered the following expection {e}. Trying to remove the prefixing backtricks if some are found. ")
+                    
+    except Exception as e:
+        if LOG:
+            logging.error(f"json repairs tool encoutered the following error: {e}")
         return input_msg
-    else:
-        repaired_content: str = repair_json(content)
-        if isinstance(repaired_content, str):
-            return AIMessage(content=repaired_content)
-        else:
-            return input_msg
+        
 

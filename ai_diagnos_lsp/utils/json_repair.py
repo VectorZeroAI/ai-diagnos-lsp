ecc90d3a5b61519c19883c4af35bc7f2cbbec70d
Null <VectorZeroAI@gmail.com>
2026-02-21 10:19:55 +0100

    fix json repairer

--- ai_diagnos_lsp/utils/json_repair.py
+++ ai_diagnos_lsp/utils/json_repair.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 from fix_busted_json import repair_json, can_parse_json # pyright: ignore
-# I use pyright in strict mode, and the developer of the library doesnt, so pyright always complains because of missing type hints
 from langchain_core.messages import AIMessage
 
 import os
 import logging
+
+
+"""
+NOTE: I basically told pyright to ignore any errors in this file, because the library I use, the fix_busted_json library,
+did not add any typehints to their code, so pyright marks it as partially unknown. 
+That is just stupid. 
+"""
 
 if os.getenv('AI_DIAGNOS_LOG') is not None:
     LOG = True
@@ -19,25 +25,25 @@ def optional_repair_json(input_msg: AIMessage) -> AIMessage:
     try:
         if LOG:
             logging.info("json repairs tool started")
-            logging.info(f"The content of AIMessage is the following : {input_msg.content}")
+            logging.info(f"The content of AIMessage is the following : {str(input_msg.content)}") # pyright: ignore
 
         if isinstance(input_msg.content, str): # pyright: ignore
             content = input_msg.content
 
         else:
-            if isinstance(input_msg.content, list):
+            if isinstance(input_msg.content, list): # pyright: ignore
 
-                if len(input_msg.content) > 0:
+                if len(input_msg.content) > 0:   # pyright: ignore
 
-                    if isinstance(input_msg.content[0], str):
-                        content = "\n".join(input_msg.content)
+                    if isinstance(input_msg.content[0], str): # pyright: ignore
+                        content = "\n".join(input_msg.content) # pyright: ignore
 
-                    elif isinstance(input_msg.content[0], dict):
+                    elif isinstance(input_msg.content[0], dict): # pyright: ignore
                         content = ""
-                        for i in input_msg.content:
-                            if i.get('type') == "text":
-                                if i.get('text') is not None:
-                                    content = content + i.get('text')
+                        for i in input_msg.content:  # pyright: ignore
+                            if i.get('type') == "text":  # pyright: ignore
+                                if i.get('text') is not None:  # pyright: ignore
+                                    content = content + i.get('text')   # pyright: ignore
                                 else:
                                     continue
                     else:
@@ -47,27 +53,28 @@ def optional_repair_json(input_msg: AIMessage) -> AIMessage:
             else:
                 return input_msg
         
-        assert content is str
+        assert isinstance(content, str)
         if can_parse_json(content):
             return input_msg
         else:
             if content.startswith('`'):
-                content.strip('`')
+                content = content.strip('`')
             if content.startswith('json'):
                 content = content[4:]
             try:
-                repaired_content: str = repair_json(content)
+                repaired_content = repair_json(content) # pyright: ignore
                 if isinstance(repaired_content, str):
                     return AIMessage(content=repaired_content)
                 else:
                     return input_msg
             except Exception as e:
                 if LOG:
-                    logging.error(f"the repair json function call encoutered the following expection {e}. Trying to remove the prefixing backtricks if some are found. ")
+                    logging.error(f"the repair json function call encoutered the following exception {e}. Trying to remove the prefixing backtricks if some are found. ")
+                return input_msg
                     
     except Exception as e:
         if LOG:
-            logging.error(f"json repairs tool encoutered the following error: {e}")
+            logging.error(f"json repairs tool encountered the following error: {e}")
         return input_msg
         
 

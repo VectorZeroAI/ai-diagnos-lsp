2f0ff3e2b80810d610827bdd9c0a5c3f518d0ca6
Null <VectorZeroAI@gmail.com>
2026-03-03 08:20:33 +0100

    further work on the overrides

--- ai_diagnos_lsp/AnalysisSubsystem/analysers/chains/PromptObjekts/CrossFileAnalysisPrompt.py
+++ ai_diagnos_lsp/AnalysisSubsystem/analysers/chains/PromptObjekts/CrossFileAnalysisPrompt.py
@@ -5,61 +5,28 @@ from typing import TYPE_CHECKING
 from langchain_core.prompts import ChatPromptTemplate
 from ai_diagnos_lsp.utils.get_overrides import get_overrides
 
-from .prompts.cross_file_analysis_system_prompt import CROSS_FILE_ANALYSIS_SYSTEM_PROMPT, cross_file_analysis_system_prompt_function
+from .prompts.cross_file_analysis_system_prompt import cross_file_analysis_system_prompt_function
 if TYPE_CHECKING:
     from default_config import user_config
 
 
-def CrossFileAnalysisPromptFactory(config: user_config | None, filetype: str | None) -> ChatPromptTemplate:
+def CrossFileAnalysisPromptFactory(config: user_config, filetype: str) -> ChatPromptTemplate:
     """
     The factory function for creating the file analysis prompt. Takes in the user config and filetype,
     returns the prompt langchain object
     """
     
+    ovrd = get_overrides(config, filetype)
+    result = ChatPromptTemplate.from_messages([
+        ("system", f"{cross_file_analysis_system_prompt_function(ovrd)}"),
+        ("human", """
+         ---- BEGIN PRIMARY FILE ----
+         {{{file_content}}}
+         ---- END PRIMARY FILE ----
 
-    if config is None or filetype is None:
-        result = ChatPromptTemplate.from_messages([
-                ("system", f"{CROSS_FILE_ANALYSIS_SYSTEM_PROMPT}"),
-                ("human", """
-                 ---- BEGIN PRIMARY FILE ----
-                 {{{file_content}}}
-                 ---- END PRIMARY FILE ----
-
-                 ---- BEGIN REFERENCE ONLY CONTEXT ----
-                 {{{context}}}
-                 ---- END REFERENCE ONLY CONTEXT
-                 """),
-                ], template_format="mustache")
-    else:
-        try:
-            ovrd = get_overrides(config, filetype)
-            prompt_f = cross_file_analysis_system_prompt_function(ovrd)
-            result = ChatPromptTemplate.from_messages([
-                    ("system", f"{prompt_f}"),
-                    ("human", """
-                     ---- BEGIN PRIMARY FILE ----
-                     {{{file_content}}}
-                     ---- END PRIMARY FILE ----
-
-                     ---- BEGIN REFERENCE ONLY CONTEXT ----
-                     {{{context}}}
-                     ---- END REFERENCE ONLY CONTEXT
-                     """),
-                    ], template_format="mustache")
-
-        except ModuleNotFoundError:
-            result = ChatPromptTemplate.from_messages([
-                    ("system", f"{CROSS_FILE_ANALYSIS_SYSTEM_PROMPT}"),
-                    ("human", """
-                     ---- BEGIN PRIMARY FILE ----
-                     {{{file_content}}}
-                     ---- END PRIMARY FILE ----
-
-                     ---- BEGIN REFERENCE ONLY CONTEXT ----
-                     {{{context}}}
-                     ---- END REFERENCE ONLY CONTEXT
-                     """),
-                    ], template_format="mustache")
-
-
+         ---- BEGIN REFERENCE ONLY CONTEXT ----
+         {{{context}}}
+         ---- END REFERENCE ONLY CONTEXT
+         """),
+        ], template_format="mustache")
     return result
--- ai_diagnos_lsp/AnalysisSubsystem/analysers/chains/PromptObjekts/CrossFileLogicAnalysisPrompt.py
+++ ai_diagnos_lsp/AnalysisSubsystem/analysers/chains/PromptObjekts/CrossFileLogicAnalysisPrompt.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
+from __future__ import annotations
 
+from typing import TYPE_CHECKING
 from langchain_core.prompts import ChatPromptTemplate
 
-from .prompts.general_logic_analysis_system_prompt import CROSS_FILE_LOGIC_ANALYSIS_PROMPT
+from ai_diagnos_lsp.utils.get_overrides import get_overrides
 
-def CrossFileLogicAnalysisPromptFactory():
+from .prompts.general_logic_analysis_system_prompt import cross_file_logic_analysis_prompt_function
 
+if TYPE_CHECKING:
+    from ai_diagnos_lsp.default_config import user_config
+
+def CrossFileLogicAnalysisPromptFactory(config: user_config, filetype: str):
+
+    ovrd = get_overrides(config, filetype)
     return ChatPromptTemplate.from_messages([
-        ("system", f"{CROSS_FILE_LOGIC_ANALYSIS_PROMPT}"),
+        ("system", f"{cross_file_logic_analysis_prompt_function(ovrd)}"),
         ("human", """
          ---- BEGIN PRIMARY FILE ----
          {{{file_content}}}
          ---- END PRIMARY FILE ----
 
-
          ---- BEGIN REFERENCE ONLY CONTEXT ----
          {{{context}}}
-         ---- END REFERENCE ONLY CONTEXT
+         ---- END REFERENCE ONLY CONTEXT ----
          """),
         ], template_format="mustache")
